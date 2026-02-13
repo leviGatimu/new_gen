9591199c3266fe0df36aabf182a16f76a3fb694e
leviGatimu <getmorelev@gmail.com>
2026-02-13 12:00:00 +0200

    Backdated commit

--- teacher/edit_assessment.php
+++ teacher/edit_assessment.php
@@ -99,7 +99,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Edit Assessment | NGA..</title>
+    <title>Edit Assessment | NGA...</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <style>
